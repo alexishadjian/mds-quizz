2337ec1656323b0411055263ceaad11a472ad03f
Alexis <alexishadjian@gmail.com>
2024-05-02 09:36:56 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView>
+        <Text>Difficulté</Text>
+        <TouchableOpacity style={styles.btn}><Text style={styles.btnTxt}>Start the quizz !</Text></TouchableOpacity>
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginHorizontal: 10,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  btn: {
+    backgroundColor: '#000',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10
+  },
+  btnTxt: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center'
+  }
 });
